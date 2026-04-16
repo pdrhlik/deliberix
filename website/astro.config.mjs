c752e6d0569e3e7808a27e83fc25ddd9f205bb52
Patrik Drhlík <patrioscraper@gmail.com>
2026-04-16 12:36:10 +0200

    Change astro dev port

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    port: 4322,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
